ebcbfd4d2d9415e6e6b2bda853eae027fd0f8741
Miha Blazin <miha.blazin@gmail.com>
2013-06-09 13:20:19 +0200

    Added YUIDoc documentation

---
+++ api.js
@@ -0,0 +1,29 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "EndView",
+        "EventManager",
+        "Game",
+        "GameView",
+        "Level",
+        "LevelSettings",
+        "Platform",
+        "PlatformSettings",
+        "Player",
+        "PlayerMove",
+        "PlayerSettings",
+        "SimpleJump",
+        "StartView"
+    ],
+    "modules": [
+        "SimpleJump"
+    ],
+    "allModules": [
+        {
+            "displayName": "SimpleJump",
+            "name": "SimpleJump",
+            "description": "A simple jumping game inspired by Doodle Jump."
+        }
+    ]
+} };
+});
